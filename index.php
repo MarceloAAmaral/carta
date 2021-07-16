7592b64195e332747126ead6f86711b7cedbaf5f
MarceloAAmaral <57374333+MarceloAAmaral@users.noreply.github.com>
2021-07-15 21:47:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<head>
+	<title>A carta</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <META NAME="DESCRIPTION" CONTENT="">
+    <META NAME="ABSTRACT" CONTENT="">
+    <META NAME="KEYWORDS" CONTENT="">
+    <META NAME="robots" CONTENT="index,follow">
+    <meta name="Classification" content="" />
+    <meta name="language" content="PT-Br" />
+    <META NAME="rating" CONTENT="general">
+    <META NAME="distribution" CONTENT="global">
+    <meta http-equiv="content-language" content="pt-br" />
+	<style>
+	body{font-size: 14pt; line-height: 300%}
+	section{width: 60%; margin:5% auto;  }
+	.link{float: right}	
+	</style>
+	
+</head>
+<section>
+	<h1> Ao Cleber</h1>
+	<article>
+		Prezado Sr. Cleber, é com grande satisfação que mantemos contato com V.S.ª para a palestra do 
+		acadêmico prof. Pedro Leite sobre o "Programa de contingência e redução da probreza no semiárido".
+		<br> Na ocasião será lançado o livro de autoria do palestrante sobre o tema apresentado.
+		
+		<br> Aguardamos a sua presença!<br>
+		
+		<br><div class='link'> Link acesso próxima carta <a href='https://paulofsnunes.github.io/carta-floripa_UNIVALI/'>clique aqui</a></div><br>
+	</article>
+</section>
